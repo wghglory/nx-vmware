d8f45becbbc0d750f21767822a292d6df94fa8da
Derek Wang <guanghuiw@vmware.com>
2023-01-02 23:28:53 +0800

    chores: clarity angular

--- apps/seed/src/app/app.module.ts
+++ apps/seed/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ClarityModule } from '@clr/angular';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ClarityModule],
   providers: [],
   bootstrap: [AppComponent],
 })
